1abe1500fdc6e7c354a0999183898d9b02a28852
Gerhard Pegel <gpegel@gmail.com>
2015-09-13 00:23:22 +0200

    My first C# console application about simple classess

--- SimpleClasses/Program.cs
+++ SimpleClasses/Program.cs
@@ -10,6 +10,49 @@ namespace SimpleClasses
     {
         static void Main(string[] args)
         {
+            Car myNewCar = new Car();
+
+            myNewCar.Make = "Honda";
+            myNewCar.Model = "Civic";
+            myNewCar.Year = 1998;
+            myNewCar.Color = "red";
+
+            Console.WriteLine("{0} - {1} - [2}",
+                myNewCar.Make,
+                myNewCar.Model,
+                myNewCar.Color);
+
+            Console.WriteLine("Car's value: {0:C}", myNewCar.determineMarketValue());
+
+            Console.ReadLine();
+        }
+
+        private static double determineMarketValue(Car _car)
+        {
+        double carValue = 100.0;
+
+        return carValue;
+
+        }
+
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public double determineMarketValue()
+        {
+            double carValue = 100.0;
+            if (this.Year > 1990)
+                carValue = 10000.0;
+            else
+                carValue = 2000.0;
+
+            return carValue;
 
         }
     }
